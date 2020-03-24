51c554df05dc9f489e7d091e151323b2bf006bda
stuart-p <stuart_palmer85@hotmail.com>
2020-03-24 23:43:57 +0000

    get api endpoints

--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,7 @@ namespace dotnetDating.api
     {
       services.AddDbContext<DataContext>(options => options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
       services.AddControllers();
+      services.AddCors();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +43,8 @@ namespace dotnetDating.api
       // app.UseHttpsRedirection();
 
       app.UseRouting();
+
+      app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
       app.UseAuthorization();
 
